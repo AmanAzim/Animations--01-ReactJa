bcbad0e7f6bf976bd92565b2a5809d57a6cbadaf
Aman <rumman.djuice1@gmail.com>
2019-04-04 20:30:47 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Transition from 'react-transition-group/Transition'
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
+import Modal2 from "./components/Modal/Modal_2";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
@@ -10,16 +11,24 @@ class App extends Component {
 
   state={
     modalIsOpen:false,
+    modalIsOpen2:false,
     showBlock: false
   };
 
   showModal=()=>{
       this.setState({modalIsOpen: true})
   };
+    showModal2=()=>{
+        this.setState({modalIsOpen2: true})
+    };
 
   closeModal=()=>{
       this.setState({modalIsOpen: false})
   };
+    closeModal2=()=>{
+        this.setState({modalIsOpen2: false})
+    };
+
     //          {this.state.showBlock? <div style={{backgroundColor:'red', width: 100, height: 100, margin:'auto'}} ></div> : null}
   render() {
     return (
@@ -49,11 +58,30 @@ class App extends Component {
 
           <br></br>
 
-        {this.state.modalIsOpen? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/> : null}
+          <Transition in={this.state.modalIsOpen} timeout={3000} mountOnEnter unmountOnExit
+                      onEnter={()=>console.log('OnEnter')}
+                      onEntering={()=>console.log('OnEntering')}
+                      onEntered={()=>console.log('OnEntered')}
+                      onExit={()=>console.log('OnExit')}
+                      onExiting={()=>console.log('OnExiting')}
+                      onExited={()=>console.log('OnExited')} >
 
-        {this.state.modalIsOpen? <Backdrop show={this.state.modalIsOpen} /> : null}
+              {(state)=>(
+                  <Modal show={state} closed={this.closeModal}/>
+              )}
+          </Transition>
+
+          {this.state.modalIsOpen? <Backdrop show /> : null}
+
+
+
+          <Modal2 show={this.state.modalIsOpen2} closed={this.closeModal2}/>
+          {this.state.modalIsOpen2? <Backdrop show /> : null}
+
 
         <button className="Button" onClick={this.showModal}>Open Modal</button>
+          <br></br>
+        <button className="Button" onClick={this.showModal2}>Open Modal 2</button>
 
         <h3>Animating Lists</h3>
         <List />
---
+++ src/components/Modal/Modal_2.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Transition from 'react-transition-group/Transition'
+
+import './Modal.css';
+
+const modal = (props) => {
+
+const animationTiming={
+    enter:400,
+    exit:1000
+};
+    return (
+        <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
+            {(state)=>{
+
+                const cssClasses = ["Modal", state==='entering'? "ModalOpen" : state==='exiting'? "ModalClose" : null];
+
+                return(
+                    <div className={cssClasses.join(' ')}>
+                        <h1>A Modal 2</h1>
+                        <button className="Button" onClick={props.closed}>Dismiss</button>
+                    </div>
+                )
+            }}
+        </Transition>
+    )
+};
+
+export default modal;
